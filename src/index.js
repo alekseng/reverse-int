8703d3686357999353f26ee350e894eaa0c1e21f
alekseng <alexvdche@mail.ru>
2023-04-30 12:25:57 +0300

    complite task all test passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (Math.sign(n) == '1') {
+       return n.toString().split('').reverse().join('')
+    } else if (Math.sign(n) == '-1') {
+       return n.toString().split('').reverse().join('').slice(0, -1)
+    }
 }
